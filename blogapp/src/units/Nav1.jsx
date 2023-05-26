937417a5f637df88b7ee00d374e272256f0cfe8f
Ppriyanka585 <ppriyankababu1@gmail.com>
2023-05-26 14:17:08 +0530

    firstapp

---
+++ blogapp/src/units/Nav1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+export default function Nav1() {
+  return (
+    <div>
+ <Container>
+      <h1 className='d-flex justify-content-center mx-5'>BLOG</h1>
+    </Container>
+
+
+    </div>
+  )
+}
